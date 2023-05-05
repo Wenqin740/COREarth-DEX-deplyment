5d585be332b9f7db3eda7f8e774b432b55eb0f92
Wenqin740 <115768945+Wenqin740@users.noreply.github.com>
2023-05-05 23:59:57 +0800

    Add files via upload

---
+++ interface/src/constants/lists.ts
@@ -0,0 +1,7 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  'https://defiprime.com/defiprime.tokenlist.json'
+]
